742d286772253e31819350ddbf7c66f1c19de07b
CS.Ferng <ferng1021@gmail.com>
2013-01-23 17:24:59 +0800

    Add all-station list. Change storage to one entry.

--- script/storage.js
+++ script/storage.js
@@ -1,23 +1,27 @@
 var INIT_MARK = ['捷運市政府站-1', '捷運市政府站-2', '龍門廣場', '捷運科技大樓站', '臺大資訊大樓', '基隆長興路口', '捷運六張犁站', '捷運公館站(2號出口)'];
+var STAR = [];
 function storageInit() {
-	chrome.storage.local.getBytesInUse('_initialized', function(size){
-		if(size != 0) return;
-		chrome.storage.local.set({'_initialized':1});
-		INIT_MARK.forEach(function(mark){
-			chrome.storage.local.set({mark:1});
-		});
+	chrome.storage.local.getBytesInUse('starred', function(size){
+		if(size != 0) {
+			chrome.storage.local.get('starred', function(item){
+				STAR = item['starred'];
+			});
+		} else {
+			chrome.storage.local.set({'starred':INIT_MARK});
+			STAR = INIT_MARK;
+		}
 	});
 }
 function storageAdd(mark) {
-	chrome.storage.local.set({mark:1});
+	STAR.push(mark);
+	chrome.storage.local.set({'starred':STAR});
 }
 function storageDel(mark) {
-	chrome.storage.local.remove(mark);
-}
-function storageGet(callback) {
-	chrome.storage.local.get(null, function(items){
-		var keys = $.map(items,function(val,key){return key;});
-		keys = $.grep(keys, function(k){return k[0] != '_';});
-		callback(keys);
-	});
+	var t = 0;
+	for(var i = 0; i < STAR.length; ++i) {
+		if(STAR[i] == mark) ++t;
+		else if(t > 0) STAR[i-t] = STAR[i];
+	}
+	for(; t > 0; --t) STAR.pop();
+	chrome.storage.local.set({'starred':STAR});
 }
--- script/ubike.js
+++ script/ubike.js
@@ -1,42 +1,54 @@
 var SRC = 'http://www.youbike.com.tw/genxml.php';
-var MARK = ['捷運市政府站-1', '捷運市政府站-2', '龍門廣場', '捷運科技大樓站', '臺大資訊大樓', '基隆長興路口', '捷運六張犁站', '捷運公館站(2號出口)'];
 function showErr(jqxhr,st,er) {
 	$('body').prepend('<p><div class=err>'+st+' '+er+'</div></p>');
 }
 function showHeader() {
-	$('body').html('<ul id=list></ul>');
+	$('#starred').html();
+	$('#all').html();
 }
-function showStation(name, addr, bike, empt, time) {
-	var ls = $('<li></li>').appendTo($('#list'));
+function showStation(lid, name, addr, bike, empt, time) {
+	var ls = $('<li></li>').appendTo($('#'+lid));
 	var div = $('<div class=item></div>').appendTo(ls);
 	var val = [name, addr, ''+bike+' 輛車 '+empt+' 空位', time];
 	var cls = ['name', 'addr', 'val', 'time'];
 	for(var i = 0; i < 4; ++i)
 		div.append('<div class='+cls[i]+'>'+val[i]+'</div>');
 }
-function showMark(xml) {
-	var ms = xml.find('marker');
+function showStarred(ms) {
 	var n = ms.length;
-	MARK.forEach(function(mark){
+	STAR.forEach(function(mark){
 		for(var i = 0; i < n; ++i) {
 			if(ms.eq(i).attr('name') == mark) {
 				var x = ms.eq(i);
-				showStation(x.attr('name'), x.attr('address'), x.attr('tot'), x.attr('sus'), x.attr('mday'));
+				showStation('starred', x.attr('name'), x.attr('address'), x.attr('tot'), x.attr('sus'), x.attr('mday'));
 				break;
 			}
 		}
 	});
 }
+function showAll(ms) {
+	var n = ms.length;
+	for(var i = 0, j = 0; i < n; ++i) {
+		var x = ms.eq(i);
+		for(j = 0; j < i; ++j)
+			if(x.attr('name') == ms.eq(j).attr('name')) break;
+		if(j == i) 
+			showStation('all', x.attr('name'), x.attr('address'), x.attr('tot'), x.attr('sus'), x.attr('mday'));
+	}
+}
 function loadXml() {
-	showHeader();
 	$.ajax({
 		url: SRC, dataType: 'text',
 		error: showErr,
 		success: function(res) {
-			showMark($(res));
+			var ms = $(res).find('marker');
+			showHeader();
+			showStarred(ms);
+			showAll(ms);
 		},
 	});
 }
 $(function(){
+	storageInit();
 	loadXml();
 });
